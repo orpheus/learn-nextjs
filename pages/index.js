a33aefee4d118320b3669b65e66b8eaec6ccf61f
orpheus <ryanachacon@gmail.com>
2018-12-17 15:32:12 -0800

    revert to next dev server

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,19 @@
 import PropTypes from 'prop-types';
-
 import Head from 'next/head';
-
 import withLayout from '../lib/withLayout';
 
-const Index = ({ user }) => (
-	<div style={{ padding: '10px 45px' }}>
+const Index = ({user}) => (
+	<div style={{padding: '10px 45px'}}>
 		<Head>
 			<title>Index page</title>
-			<meta name="description" content="This is the description of the Index page" />
+			<meta name="description" content="This is the description of the Index page"/>
 		</Head>
 		<p>Content on Index page</p>
-		<p>Email: {user.email}</p>
+		<p>Email: {user ? user.email : 'remove.this@test.com'}</p>
 	</div>
 );
 
-Index.getInitialProps = async ({ query }) => ({ user: query.user });
+Index.getInitialProps = async ({query}) => ({user: query.user});
 
 Index.propTypes = {
 	user: PropTypes.shape({
