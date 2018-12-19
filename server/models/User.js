c33b49f343f8ce4878876f4e3432f2af18ec9e7c
orpheus <ryanachacon@gmail.com>
2018-12-19 10:22:10 -0800

    3.6+7 user class, signInSignUp, generate slug

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,9 @@
+import _ from 'lodash';
 import mongoose from 'mongoose';
 
-const { Schema } = mongoose;
+import generateSlug from '../utils/slugify';
+
+const {Schema} = mongoose;
 
 const mongoSchema = new Schema({
 	googleId: {
@@ -43,6 +46,62 @@ const mongoSchema = new Schema({
 		type: String,
 	},
 });
+
+class UserClass {
+	static publicFields() {
+		return [
+			'id',
+			'displayName',
+			'email',
+			'avatarUrl',
+			'slug',
+			'isAdmin',
+			'isGithubConnected',
+		];
+	}
+
+	static async signInOrSignUp({googleId, email, googleToken, displayName, avatarUrl,}) {
+		const user = await this.findOne({googleId}).select(UserClass.publicFields().join(' '));
+
+		if (user) {
+			const modifier = {};
+
+			if (googleToken.accessToken) {
+				modifier.access_token = googleToken.accessToken;
+			}
+
+			if (googleToken.refreshToken) {
+				modifier.refresh_token = googleToken.refreshToken;
+			}
+
+			if (_.isEmpty(modifier)) {
+				return user;
+			}
+
+			await this.updateOne({googleId}, {$set: modifier});
+
+			return user;
+		}
+
+		const slug = await generateSlug(this, displayName);
+		const userCount = await this.find().countDocuments();
+
+		const newUser = await this.create({
+			createdAt: new Date(),
+			googleId,
+			email,
+			googleToken,
+			displayName,
+			avatarUrl,
+			slug,
+			isAdmin: userCount === 0,
+		});
+
+		return _.pick(newUser, UserClass.publicFields());
+	}
+}
+
+mongoSchema.loadClass(UserClass);
 
 const User = mongoose.model('User', mongoSchema);
 
