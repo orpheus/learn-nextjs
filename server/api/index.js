c8a1c75dc53a4b2dc2b1028dd4d1da6eb62a444b
orpheus <ryanachacon@gmail.com>
2019-01-04 08:49:43 -0800

    5.3 add pub/cust api endpoints

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,7 @@ import customerApi from './customer';
 import adminApi from './admin';
 
 export default function api(server) {
-	// server.use('/api/v1/public', publicApi);
-	// server.use('/api/v1/customer', customerApi);
+	server.use('/api/v1/public', publicApi);
+	server.use('/api/v1/customer', customerApi);
 	server.use('/api/v1/admin', adminApi);
 }
---
+++ server/api/public.js
@@ -0,0 +1,48 @@
+import express from 'express';
+
+import Book from '../models/Book';
+import Chapter from '../models/Chapter';
+// import User from '../models/User';
+import logger from '../logs'
+
+const router = express.Router();
+
+router.get('/books', async (req, res) => {
+	logger.info('/books')
+	try {
+		const books = await Book.list();
+		res.json(books);
+	} catch (err) {
+		res.json({ error: err.message || err.toString() });
+	}
+});
+
+router.get('/books/:slug', async (req, res) => {
+	logger.info('/books/:slug')
+	try {
+		const book = await Book.getBySlug({ slug: req.params.slug, userId: req.user && req.user.id });
+		res.json(book);
+	} catch (err) {
+		res.json({ error: err.message || err.toString() });
+	}
+});
+
+router.get('/get-chapter-detail', async (req, res) => {
+	logger.info('/get-chapter-detail')
+	const { bookSlug, chapterSlug } = req.query;
+	let chapter
+	try {
+		chapter = await Chapter.getBySlug({
+			bookSlug,
+			chapterSlug,
+			userId: req.user && req.user.id,
+			isAdmin: req.user && req.user.isAdmin,
+		});
+	} catch (err) {
+		res.json({ error: err.message || err.toString() });
+	}
+	res.json(chapter);
+
+});
+
+export default router;
