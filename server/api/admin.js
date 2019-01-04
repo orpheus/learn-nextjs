6104ec5c312ce27bc6992351418348bcb55d4e10
orpheus <ryanachacon@gmail.com>
2019-01-04 12:38:55 -0800

    6.3 add server api admin routes

--- server/api/admin.js
+++ server/api/admin.js
@@ -1,6 +1,9 @@
 import express from 'express';
 
 import Book from '../models/Book';
+import { getContent, getRepos } from '../github';
+import User from '../models/User';
+import logger from '../logs';
 
 const router = express.Router();
 
@@ -23,5 +26,68 @@ router.get('/books', async (req, res) => {
 });
 
 // more routes
+router.post('/books/add', async (req, res) => {
+	try {
+		const book = await Book.add(Object.assign({ userId: req.user.id }, req.body));
+		res.json(book);
+	} catch (err) {
+		logger.error(err);
+		res.json({ error: err.message || err.toString() });
+	}
+});
+
+router.post('/books/edit', async (req, res) => {
+	try {
+		const editedBook = await Book.edit(req.body);
+		res.json(editedBook);
+	} catch (err) {
+		res.json({ error: err.message || err.toString() });
+	}
+});
+
+router.get('/books/detail/:slug', async (req, res) => {
+	try {
+		const book = await Book.getBySlug({ slug: req.params.slug });
+		res.json(book);
+	} catch (err) {
+		res.json({ error: err.message || err.toString() });
+	}
+});
+
+router.post('/books/sync-content', async (req, res) => {
+	const { bookId } = req.body;
+
+	const user = await User.findById(req.user._id, 'isGithubConnected githubAccessToken');
+
+	if (!user.isGithubConnected || !user.githubAccessToken) {
+		res.json({ error: 'Github not connected' });
+		return;
+	}
+
+	try {
+		await Book.syncContent({ id: bookId, githubAccessToken: user.githubAccessToken });
+		res.json({ done: 1 });
+	} catch (err) {
+		logger.error(err);
+		res.json({ error: err.message || err.toString() });
+	}
+});
+
+router.get('/github/repos', async (req, res) => {
+	const user = await User.findById(req.user._id, 'isGithubConnected githubAccessToken');
+
+	if (!user.isGithubConnected || !user.githubAccessToken) {
+		res.json({ error: 'Github is not connected' });
+		return;
+	}
+
+	try {
+		const response = await getRepos({ accessToken: user.githubAccessToken });
+		res.json({ repos: response.data });
+	} catch (err) {
+		logger.error(err);
+		res.json({ error: err.message || err.toString() });
+	}
+});
 
 export default router;
